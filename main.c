3b4c6bc7bdc4b4b0bfe1c201f8080a4f4a29947c
Ba2hanKaya <baa2han.kay@gmail.com>
2026-08-24 15:18:01 +0300

    fix: fixed function declaration without prototypes, declaration showing a global var, and implicit conversion warnings.

--- main.c
+++ main.c
@@ -12,15 +12,15 @@ uint64_t x = 12345; //main seed
 static uint64_t s[NUM_THREADS * 4]; //for holding each thread's 4 seeds
 uint64_t result_array[NUM_THREADS + 1][(DECK_SIZE - NUM_SUITS)/2 + 1] = {{0}}; //2D array for holding each thread's score, plus their sum at the last row. (Deck size - number of tens) / 2 gives the number of possible matches and therefore scores, + 1 for no match (a score of 0)
 
-uint64_t splitmix_next() {
+uint64_t splitmix_next(void) {
 	uint64_t z = (x += UINT64_C(0x9E3779B97F4A7C15));
 	z = (z ^ (z >> 30)) * UINT64_C(0xBF58476D1CE4E5B9);
 	z = (z ^ (z >> 27)) * UINT64_C(0x94D049BB133111EB);
 	return z ^ (z >> 31);
 }
 
-static inline uint64_t rotl(const uint64_t x, int k) {
-	return (x << k) | (x >> (64 - k));
+static inline uint64_t rotl(const uint64_t seed, int k) {
+	return (seed << k) | (seed >> (64 - k));
 }
 
 uint64_t next(int i) {
@@ -60,8 +60,8 @@ uint32_t random_range(uint32_t min, uint32_t max, int thread_id) {
 }
 
 void fisher_yates_shuffle(uint8_t arr[], int thread_id) {
-    for (int i = DECK_SIZE - 1; i > 0; i--) {
-        int j = random_range(0, i, thread_id); 
+    for (uint32_t i = DECK_SIZE - 1; i > 0; i--) {
+        uint32_t j = random_range(0, i, thread_id); 
 
         uint8_t temp = arr[j];
         arr[j] = arr[i];
@@ -84,7 +84,7 @@ void assert_deck_is_valid(uint8_t arr[])
   }
 }
 
-int score_array(uint8_t arr[])
+uint8_t score_array(uint8_t arr[])
 {
   int score = 0;
   int count = 0;
@@ -154,25 +154,28 @@ void* worker(void* arg)
 {
   struct ThreadArgs* args = (struct ThreadArgs*)arg;
   int thread_id = args->id;
-  int num_runs = args->num_sim;
+  uint32_t num_runs = args->num_sim;
+
   uint8_t deck[DECK_SIZE];
   for(int j = 0; j < 4; j++)
   {
-    for(int i = 0; i < DECK_SIZE/4; i++)
+    for(uint8_t i = 0; i < DECK_SIZE/4; i++)
     {
-      deck[j*13 + i] = i + 1; 
+      deck[j * 13 + i] = i + 1; 
     }
   }
-  for(int i = 0; i < num_runs; i++)
+
+  for(uint32_t i = 0; i < num_runs; i++)
   {
     fisher_yates_shuffle(deck, thread_id);
     uint8_t score = score_array(deck);
     result_array[thread_id][score]++;
   }
+
   return NULL;
 }
 
-void initialize_xoshiro_seeds()
+void initialize_xoshiro_seeds(void)
 {
   for(int i = 0; i < NUM_THREADS * 4; i++)
   {
@@ -180,7 +183,7 @@ void initialize_xoshiro_seeds()
   }
 }
 
-void init()
+int main(void)
 {
   initialize_xoshiro_seeds();
 }
@@ -190,8 +193,11 @@ int main()
 {
   init();
   pthread_t threads[NUM_THREADS];
+
   struct ThreadArgs args[NUM_THREADS];
+
   int modulo_num = num_simulations % NUM_THREADS;
+
   for(int i = 0; i < NUM_THREADS; i++)
   {
     args[i].id = i;
@@ -200,19 +206,21 @@ int main()
     int return_code = pthread_create(&threads[i], NULL, worker, &args[i]); 
     if(return_code != 0)
     {
-      fprintf(stderr, "Thread no %d creation failed with return code: %d\n", i, return_code);
+      (void)fprintf(stderr, "Thread no %d creation failed with return code: %d\n", i, return_code);
       return 1;
     }
   }
+
   for(int i = 0; i < NUM_THREADS; i++)
   {
     int join_ret_code = pthread_join(threads[i], NULL);
     if(join_ret_code != 0)
     {
-      fprintf(stderr, "Thread no %d join call failed with return code: %d\n", i, join_ret_code);
+      (void)fprintf(stderr, "Thread no %d join call failed with return code: %d\n", i, join_ret_code);
       return 1;
     }
   }
+
   for(int i = 0; i < NUM_THREADS; i++)
   {
     for(int j = 0; j < 25; j++)
@@ -220,13 +228,15 @@ int main()
       result_array[NUM_THREADS][j] += result_array[i][j];
     }
   }
+
   uint64_t count = 0;
   for(int j = 0; j < 25; j++)
   {
-    fprintf(stdout, "%ld ", result_array[NUM_THREADS][j]);
+    (void)fprintf(stdout, "%ld ", result_array[NUM_THREADS][j]);
     count += result_array[NUM_THREADS][j];
   }
-  fprintf(stdout, "\nTotal sim ran: %ld", count);
+
+  (void)fprintf(stdout, "\nTotal sim ran: %ld\n", count);
   return 0;
 }
 
